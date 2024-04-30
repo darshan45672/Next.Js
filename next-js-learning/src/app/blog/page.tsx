d3ff62f2cf02d9c0cc45ceb9ef9cfe32004777ce
darshan45672 <drshnbhandary@gmail.com>
2024-04-30 10:59:36 +0530

    added multi folder nested routing

---
+++ next-js-learning/src/app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function blog(){ 
+    return (
+        <div>
+            <h1>Blog</h1>
+            <p>This is the blog page darshan</p>
+        </div>
+    );
+}
---
+++ next-js-learning/src/app/blog/second/page.tsx
@@ -0,0 +1,8 @@
+export default function second(){
+    return (
+        <div>
+            <h1>Second</h1>
+            <p>This is the second page darshan</p>
+        </div>
+    );
+}
